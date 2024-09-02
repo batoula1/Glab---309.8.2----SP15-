f8eb1d7bce8b7cc0cd6e7dd3968933f9e59c434b
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-09-01 22:45:57 -0400

    commit2

--- src/main/java/repository/RoleRepository.java
+++ src/main/java/repository/RoleRepository.java
@@ -1,4 +1,10 @@
 package repository;
 
-public class RoleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long>
+{
+
+    Role findByName(String name);
 }
+
--- src/main/java/repository/StudentRepository.java
+++ src/main/java/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package repository;
 
-public class StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long>
+{
+
+    Student findByEmail(String email);
 }
